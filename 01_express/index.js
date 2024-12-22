6a400623787e7624a2d62301b8ef67f11cfb534a
Dewashish Gope <dewashishgope99@gmail.com>
2024-12-22 20:40:22 +0530

    Backend CRUD App

---
+++ 01_express/index.js
@@ -0,0 +1,64 @@
+import express from 'express'
+
+const app = express()
+
+const port = 3000
+
+app.use(express.json())
+
+let teaData = []
+let nextId = 1
+
+//Add Tea Data
+
+app.post('/teas', (req, res) => {
+
+    const {name, price} = req.body
+    const newTea = {id: nextId++, name, price}
+    teaData.push(newTea)
+    res.status(201).send(newTea)
+})
+
+//View Tea Data
+
+app.get('/teas', (req,res) => {
+    res.status(200).send(teaData)
+})
+
+//View Tea Data As per Id
+
+app.get('/teas/:id', (req,res) => {
+    const tea = teaData.find( t => t.id === parseInt(req.params.id) )
+    if(!tea){
+        return res.status(404).send('Tea not found')
+    }
+    res.status(200).send(tea)
+})
+
+//update Tea Data
+
+app.put('/teas/:id', (req, res) => {
+    const tea = teaData.find( t => t.id === parseInt(req.params.id) )
+    if(!tea){
+        return res.status(404).send('Tea not found')
+    }
+    const {name, price} = req.body
+    tea.name = name
+    tea.price = price
+    res.status(200).send(tea)
+})
+
+//Delete Tea Data
+
+app.delete('/teas/:id', (req, res) => {
+    const index = teaData.findIndex(t => t.id === parseInt(req.params.id))
+    if (index === -1) {
+        return res.status(404).send('tea not found')
+    }
+    teaData.splice(index, 1)
+    return res.status(200).send('deleted')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running at port ${port}...`)
+})
